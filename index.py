47c2166692f22940f6024ce973df09bd7324fe6e
kipchirchir kibet <kibet780@gmail.com>
2023-11-28 16:50:40 +0300

    added new changes

--- index.py
+++ index.py
@@ -1,20 +1,19 @@
-import fitz  # PyMuPDF
+import subprocess
 
-def pdf_to_html(pdf_path, output_path):
-    # Open the PDF file
-    pdf_document = fitz.open(pdf_path)
+def html_to_pdf(html_path, output_path):
+    command = f"wkhtmltopdf {html_path} {output_path}"
+    subprocess.run(command, shell=True)
 
-    # Initialize HTML content
-    html_content = ""
+# Example usage
+html_path = "/Users/mac/apps/pdf/files/JS3.pdf"
+pdf_output_path = "/Users/mac/apps/pdf/files/kips.pdf"
 
-    # Iterate through pages and extract text
-    for page_number in range(pdf_document.page_count):
-        page = pdf_document[page_number]
-        html_content += page.get_text("html")
+html_to_pdf(html_path, pdf_output_path)
 
-    # Write HTML content to the output file
-    with open(output_path, "w", encoding="utf-8") as html_file:
-        html_file.write(html_content)
+
+
+
+
 
 # Example usage
 pdf_path = "/Users/mac/apps/pdf/files/JS3.pdf"
