559a6a29b04469fdc94754d4ee537bf28b2e5837
kipchirchir kibet <kibet780@gmail.com>
2023-11-28 16:54:02 +0300

    added new changes

--- index.py
+++ index.py
@@ -5,17 +5,7 @@ def html_to_pdf(html_path, output_path):
     subprocess.run(command, shell=True)
 
 # Example usage
-html_path = "/Users/mac/apps/pdf/files/JS3.pdf"
-pdf_output_path = "/Users/mac/apps/pdf/files/kips.pdf"
+html_path = "/srv/pdf/files/JS3.pdf"
+pdf_output_path = "/srv/pdf/files/kips.pdf"
 
 html_to_pdf(html_path, pdf_output_path)
-
-
-
-
-
-
-# Example usage
-pdf_path = "/Users/mac/apps/pdf/files/JS3.pdf"
-output_path = "/Users/mac/apps/pdf/files/kips.html"
-pdf_to_html(pdf_path, output_path)
